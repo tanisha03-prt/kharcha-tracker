e29148a8f604d89e20cd39c9286c925b0ec7a140
Tanisha <tanishasingh0302@gmail.com>
2026-08-18 12:22:23 +0530

    Added Goal.js

---
+++ backend/models/Goal.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const goalSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    targetAmount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    savedAmount: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+
+    targetDate: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Goal", goalSchema);
